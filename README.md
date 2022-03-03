4fb9e34da0bb1da5091f210360a349c5931be6c0
Gobind Puniani <gobindpuniani@gmail.com>
2022-03-02 23:40:39 -0800

    Added contract and migrations

---
+++ README.md
@@ -0,0 +1,3 @@
+# MakeCoin
+
+## Contract Address
---
+++ contracts/ShoutoutContract.sol
@@ -0,0 +1,33 @@
+
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+// contract name must match file name: StarContract?
+// Also fix 2_deploy_contracts.js
+contract ShoutoutContract is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // Items and units: stars?; second one is like BTC or ETH
+    constructor() ERC721("Shoutouts", "SHOUTS") {
+
+    }
+
+    function awardItem(address recipient, string memory metadata)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, metadata);
+
+        return newItemId;
+    }
+}
